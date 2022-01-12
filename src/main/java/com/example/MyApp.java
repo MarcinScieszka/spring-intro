46810ff053c243b06f1705b3bdaf9c5446f4bac4
MarcinScieszka <79609951+MarcinScieszka@users.noreply.github.com>
2022-01-12 18:23:55 +0100

    add Java configuration

--- src/main/java/com/example/MyApp.java
+++ src/main/java/com/example/MyApp.java
@@ -1,21 +1,20 @@
 package com.example;
 
-import org.springframework.context.support.ClassPathXmlApplicationContext;
-
-import java.util.Random;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class MyApp {
 
-    public static void main(String[] args) {
-        ClassPathXmlApplicationContext applicationContext = new ClassPathXmlApplicationContext("applicationContext.xml");
 
-        MessageService messageService = applicationContext.getBean("messageService", MessageService.class);
-        MessageService messageService2 = applicationContext.getBean("messageService", MessageService.class);
+    public static void main(String[] args) {
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(MessageServiceConfig.class);
+
+        MessageService messageService = applicationContext.getBean(RandomTextMessageService.class);
+        MessageService messageService2 = applicationContext.getBean(RandomTextMessageService.class);
 
         System.out.println(messageService.hashCode() + " --- " + messageService2.hashCode());
 
         System.out.println(messageService.getMessage());
 
-        applicationContext.close();
     }
 }
--- src/main/java/com/example/RandomTextMessageService.java
+++ src/main/java/com/example/RandomTextMessageService.java
@@ -2,7 +2,6 @@ package com.example;
 
 import org.springframework.stereotype.Component;
 
-import java.util.ArrayList;
 import java.util.Random;
 
 @Component("randomMessageService")
